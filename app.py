39a139a6db50d01405fc09d87cd341a1f772dc86
Dayan <dayanithaclaros@hotmail.com>
2023-11-08 21:34:26 -0600

    Crud

--- app.py
+++ app.py
@@ -14,12 +14,26 @@ class Task(db.Model):
 #ROUTES
 @app.route('/')
 def home():
-     return render_template('index.html')
- 
+    tasks = Task.query.all()
+    return render_template('index.html', tasks = tasks)
+
 @app.route('/create-task', methods=['POST'])
 def create():
     new_task = Task(content=request.form['content'], done= False)
     db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/done/<id>')
+def done(id):
+    task = Task.query.filter_by(id=int(id)).first()
+    task.done = not(task.done)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    Task.query.filter_by(id=int(id)).delete()
     db.session.commit()
     return redirect(url_for('home'))
 
